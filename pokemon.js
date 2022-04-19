8999aacecfe26aa89bb0ce3b1ff1b8df30f3a1a5
FedericoSanchez-Ksquare <102171026+FedericoSanchez-Ksquare@users.noreply.github.com>
2022-04-18 21:12:38 -0500

    Added function backgroundTypes
    This to simplify code avoiding three big switch cases, and deleted some useless variables and changed some variable names to be more descriptive

--- pokemon.js
+++ pokemon.js
@@ -7,8 +7,7 @@ const pokeDefense = document.getElementById("poke-defenseValue");
 const pokeSpeed = document.getElementById("poke-speedValue");
 const pokemonID = document.querySelector(".aSelec");
 const pokeContainer = document.getElementById("poke-dex1");
-const test = document.getElementById("poke-card1");
-const test2 = document.getElementById("idTest");
+const onePoke = document.getElementById("poke-card1");
 const pokeEl = document.querySelector(".test");
 const alone = document.querySelector(".poke-search");
 
@@ -29,7 +28,7 @@ const generatePokemon = async (id) => {
 
 const refresh = () => {
   document.getElementById("pokemon").value = "";
-  test.style.display = "none";
+  onePoke.style.display = "none";
   pokeContainer.style.display = "";
 };
 
@@ -39,7 +38,6 @@ const createPokeCard = (data) => {
   const pokeEl = document.createElement("div");
   pokeEl.classList.add("poke-card");
   pokeEl.classList.add("test");
-  pokeEl.setAttribute("id", "idTest");
   pokeEl.classList.add("visibility2");
 
   if (types.length > 1) {
@@ -68,7 +66,7 @@ const createPokeCard = (data) => {
     case "fighting":
       pokeEl.setAttribute("style", "background-color: #C22E28;");
       break;
-    case "posion":
+    case "poison":
       pokeEl.setAttribute("style", "background-color: #A33EA1;");
       break;
     case "ground":
@@ -156,7 +154,7 @@ const getPokemonData = async () => {
   if (pokemon === "" && pokemon.value === undefined) {
     getPokemonDataRandom();
   } else {
-    test.style.display = "block";
+    onePoke.style.display = "block";
     pokeContainer.style.display = "none";
     let types = data.types;
     pokeName.innerText = `${data.name}`;
@@ -167,62 +165,8 @@ const getPokemonData = async () => {
       pokeTypes.innerText = `${data.types[0].type.name}`;
     }
 
-    switch (types[0].type.name) {
-      case "normal":
-        alone.setAttribute("style", "background-color: #A8A77A;");
-        break;
-      case "fire":
-        alone.setAttribute("style", "background-color: #EE8130;");
-        break;
-      case "water":
-        alone.setAttribute("style", "background-color: #6390F0;");
-        break;
-      case "electric":
-        alone.setAttribute("style", "background-color: #F7D02C;");
-        break;
-      case "grass":
-        alone.setAttribute("style", "background-color: #7AC74C;");
-        break;
-      case "ice":
-        alone.setAttribute("style", "background-color: #96D9D6;");
-        break;
-      case "fighting":
-        alone.setAttribute("style", "background-color: #C22E28;");
-        break;
-      case "posion":
-        alone.setAttribute("style", "background-color: #A33EA1;");
-        break;
-      case "ground":
-        alone.setAttribute("style", "background-color: #E2BF65;");
-        break;
-      case "flying":
-        alone.setAttribute("style", "background-color: #A98FF3;");
-        break;
-      case "psychic":
-        alone.setAttribute("style", "background-color: #F95587;");
-        break;
-      case "bug":
-        alone.setAttribute("style", "background-color: #A6B91A;");
-        break;
-      case "rock":
-        alone.setAttribute("style", "background-color: #B6A136;");
-        break;
-      case "ghost":
-        alone.setAttribute("style", "background-color: #735797;");
-        break;
-      case "dragon":
-        alone.setAttribute("style", "background-color: #6F35FC;");
-        break;
-      case "dark":
-        alone.setAttribute("style", "background-color: #705746;");
-        break;
-      case "steel":
-        alone.setAttribute("style", "background-color: #B7B7CE;");
-        break;
-      case "fairy":
-        alone.setAttribute("style", "background-color: #D685AD;");
-        break;
-    }
+    backgroundTypes(types);
+
     pokeAttack.innerText = `${data.stats[1].base_stat}`;
     pokeHp.innerText = `${data.stats[0].base_stat}`;
     pokeDefense.innerText = `${data.stats[2].base_stat}`;
@@ -232,7 +176,7 @@ const getPokemonData = async () => {
 };
 
 const getPokemonDataRandom = async () => {
-  test.style.display = "block";
+  onePoke.style.display = "block";
   pokeContainer.style.display = "none";
   let id = Math.floor(Math.random() * 898);
   let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -246,6 +190,16 @@ const getPokemonDataRandom = async () => {
     pokeTypes.innerText = `${data.types[0].type.name}`;
   }
 
+  backgroundTypes(types);
+
+  pokeAttack.innerText = `${data.stats[1].base_stat}`;
+  pokeHp.innerText = `${data.stats[0].base_stat}`;
+  pokeDefense.innerText = `${data.stats[2].base_stat}`;
+  pokeSpeed.innerText = `${data.stats[5].base_stat}`;
+  pokemonID.href = `https:/www.wikidex.net/wiki/${data.name}`;
+};
+
+function backgroundTypes(types) {
   switch (types[0].type.name) {
     case "normal":
       alone.setAttribute("style", "background-color: #A8A77A;");
@@ -268,7 +222,7 @@ const getPokemonDataRandom = async () => {
     case "fighting":
       alone.setAttribute("style", "background-color: #C22E28;");
       break;
-    case "posion":
+    case "poison":
       alone.setAttribute("style", "background-color: #A33EA1;");
       break;
     case "ground":
@@ -302,12 +256,6 @@ const getPokemonDataRandom = async () => {
       alone.setAttribute("style", "background-color: #D685AD;");
       break;
   }
-
-  pokeAttack.innerText = `${data.stats[1].base_stat}`;
-  pokeHp.innerText = `${data.stats[0].base_stat}`;
-  pokeDefense.innerText = `${data.stats[2].base_stat}`;
-  pokeSpeed.innerText = `${data.stats[5].base_stat}`;
-  pokemonID.href = `https:/www.wikidex.net/wiki/${data.name}`;
-};
+}
 
 fetchPokemons(12);
